4bebe2184ed6df63f6e9e694298473420a33c356
Roman Beya <roman.beya@ocsbstudent.com>
2017-09-12 13:30:40 -0400

    Initial version

---
+++ hello_world_ui.py
@@ -0,0 +1,9 @@
+# Created by: Roman Beya
+# Created on: 12-Sep-2017
+# Created for class: ICS3U
+# This displays a "Hello, World!" label on a user interface
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
